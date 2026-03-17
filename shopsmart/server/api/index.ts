0d63d24c306957935c13f36ff4674b46a83cac26
Taj-786 <tajuddinshaik786r@gmail.com>
2026-03-17 22:58:05 +0530

    added a new workflow

--- shopsmart/server/api/index.ts
+++
@@ -1,9 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "http";
-import app from "../src/app";
-
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  // Express apps are compatible with Node request/response.
-  // Vercel provides (req, res) for serverless functions.
-  return app(req as any, res as any);
-}
-
